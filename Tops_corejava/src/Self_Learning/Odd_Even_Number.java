f00bbd82c7517eb6bedc7411547ad7c58648a5c3
radhekrushn <118706055+radhekrushn@users.noreply.github.com>
2023-09-11 21:23:01 +0530

    Add files via upload

---
+++ Tops_corejava/src/Self_Learning/Odd_Even_Number.java
@@ -0,0 +1,55 @@
+package Self_Learning;
+
+import java.util.Scanner;
+
+public class Odd_Even_Number {
+
+
+	public static void main(String[] args) {
+		
+		int num = 100;
+		
+		for(int a=1;a<=num;a++)
+		{
+		 if(a%2==0)
+		 {
+			 System.out.println(a+": Even number ");
+		 }
+		 else
+		 {
+			 System.out.println(a+": Odd number");
+		 }
+		}
+	
+		/*
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if(num%2 == 0)
+        {
+        	System.out.println(num + " is even number");
+        }
+        else
+        {
+        	System.out.println(num + " is odd number");
+
+        }
+       */     
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
