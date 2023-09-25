2cc8b0c8b7da847db7cf6b4c641795a35622e2ae
radhekrushn <118706055+radhekrushn@users.noreply.github.com>
2023-09-25 22:50:36 +0530

    Add files via upload

---
+++ Tops_corejava/src/Oops/constructor.java
@@ -0,0 +1,44 @@
+package Oops;
+
+class product
+	{	
+	
+	
+	 int id;
+	 int price;
+	 int quantity;
+	 String name;
+	 String company;
+	 
+  
+    product(int a,int b,int c,String d,String e) // constructor
+    {
+    	id = a;
+    	price = b;
+    	quantity = c;
+    	name = d;
+    	company = e;
+    			
+    	
+    }
+    
+    public void show()
+    {
+    	System.out.println(id+" "+price+" "+quantity+" "+name+" "+company);
+    }
+
+}
+
+
+public class constructor {
+	
+	public static void main(String[] args) {
+		
+		product pro = new product(10,200,23,"cello pen","Adani");
+		pro.show();
+		
+				
+
+	}
+
+}
